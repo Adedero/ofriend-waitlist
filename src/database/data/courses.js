2f8e26ca20692c97efcff28cba7f2057ea250427
Adedero <118962395+Adedero@users.noreply.github.com>
2024-10-04 08:48:08 +0100

    Made minor corrections

--- src/database/data/courses.js
+++ src/database/data/courses.js
@@ -2,7 +2,7 @@ const courses = [
   {
     title: "Graphic Design",
     description: "Adobe Photoshop | CorelDraw",
-    tutor: "Ofriend Edu",
+    tutor: "Ofriend",
     price: 200,
     discountedPrice: "0.00",
     image: "/assets/images/courses/graphic-design.jpg"
@@ -10,7 +10,7 @@ const courses = [
   {
     title: "Product Design",
     description: "Adobe Illustrator | Figma",
-    tutor: "Ofriend Edu",
+    tutor: "Ofriend",
     price: 250,
     discountedPrice: "0.00",
     image: "/assets/images/courses/product-design.jpg"
@@ -18,10 +18,18 @@ const courses = [
   {
     title: "Web Development",
     description: "HTML | CSS | JavaScript",
-    tutor: "Ofriend Edu",
+    tutor: "Ofriend",
     price: 350,
     discountedPrice: "0.00",
     image: "/assets/images/courses/web-dev.jpg"
+  },
+  {
+    title: "Data Analysis",
+    description: "MS Excel | Python",
+    tutor: "Ofriend",
+    price: 280,
+    discountedPrice: "0.00",
+    image: "/assets/images/courses/data-analysis.png"
   }
 ]
 
